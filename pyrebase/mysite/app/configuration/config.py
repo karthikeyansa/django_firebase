922aac42039ead7ef5d6d26ef103eb41df888ece
karthikeyansa <karthikeyansa39@gmail.com>
2020-06-13 10:54:27 +0530

    creds

---
+++ pyrebase/mysite/app/configuration/config.py
@@ -0,0 +1,12 @@
+def config():
+    return {
+    'apiKey': "AIzaSyAOJGjT-1Umc7i6SuGBVomJU-nWfUYhGs4",
+    'authDomain': "django-c9216.firebaseapp.com",
+    'databaseURL': "https://django-c9216.firebaseio.com",
+    'projectId': "django-c9216",
+    'storageBucket': "django-c9216.appspot.com",
+    'messagingSenderId': "424754483306",
+    'appId': "1:424754483306:web:b14e7a7a39d7ce849f3a13",
+    'measurementId': "G-0D508PHZW3",
+    'serviceAccount':"C:\django\django\mysite\{app}\creds.json".format(app = "app")
+    }
--- pyrebase/mysite/app/views.py
+++ pyrebase/mysite/app/views.py
@@ -4,18 +4,9 @@ from django.contrib import auth as Auth
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 from datetime import datetime
-config ={
-    'apiKey': "AIzaSyAOJGjT-1Umc7i6SuGBVomJU-nWfUYhGs4",
-    'authDomain': "django-c9216.firebaseapp.com",
-    'databaseURL': "https://django-c9216.firebaseio.com",
-    'projectId': "django-c9216",
-    'storageBucket': "django-c9216.appspot.com",
-    'messagingSenderId': "424754483306",
-    'appId': "1:424754483306:web:b14e7a7a39d7ce849f3a13",
-    'measurementId': "G-0D508PHZW3"
-}
-firebase = pyrebase.initialize_app(config)
+from .configuration.config import config
 
+firebase = pyrebase.initialize_app(config())
 auth = firebase.auth()
 database = firebase.database()
 
@@ -23,8 +14,11 @@ def signin(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
+        print(email,password)
         user = auth.sign_in_with_email_and_password(email, password)
+        print(user)
         if user:
+            user = auth.refresh(user['refreshToken'])
             session_id = user['idToken']
             request.session['uid'] = str(session_id)
             return redirect(welcome,email = email)
@@ -49,7 +43,7 @@ def newuser(request):
     return render(request,'mysite/signup.html')
 
 def welcome(request,email):
-        return render(request,'mysite/welcome.html',{'email':email})
+    return render(request,'mysite/welcome.html',{'email':email})
 def logout(request):
     del request.session['uid']
     messages.add_message(request, messages.SUCCESS, 'Logged out successfully.')
@@ -70,6 +64,16 @@ def post_report(request):
             }
             database.child("users").child(user).child("reports").child(str(timestamp)).set(data)
             return redirect(welcome,email = email)
+        idtoken = request.session['uid']
+        user = auth.get_account_info(idtoken)
+        email, user = user['users'][0]['email'], user['users'][0]['localId']
+        data = sorted(list(database.child("users").child(user).child("reports").shallow().get().val()), reverse=True)
+        tasks = []
+        for i in data:
+            task = database.child("users").child(user).child("reports").child(i).get().val()
+            tasks.append(task)
+        print(tasks)
+        return render(request, 'mysite/welcome.html', {'email': email, 'tasks': tasks})
     except:
         messages.add_message(request, messages.SUCCESS, 'Invalid credentials Login again.')
         return redirect(signin)
